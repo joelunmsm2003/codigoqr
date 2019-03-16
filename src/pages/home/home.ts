15a42b078fe65243eefc4590eb52c1a6e594d48b
Your Name <you@example.com>
2019-03-16 13:06:00 -0500

    x

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,122 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+
+	scanSub:any;
+
+  constructor(private qrScanner: QRScanner,public navCtrl: NavController) {
+
+
+
+
+
+ 
 
   }
+
+
+
+   ionViewDidLoad(){
+
+   	  	window.document.querySelector('ion-app').classList.add('transparentBody')
+
+	this.qrScanner.prepare()
+    .then((status: QRScannerStatus) => {
+     if (status.authorized) {
+       // camera permission was granted
+
+       alert('Autorizado....')
+       // start scanning
+       this.scanSub = this.qrScanner.scan().subscribe((text: string) => {
+         alert('Scanned something'+ text);
+
+
+       });
+
+       this.qrScanner.hide(); // hide camera preview
+	   this.scanSub.unsubscribe(); // stop scanning
+
+
+     } else if (status.denied) {
+
+       // camera permission was permanently denied
+       // you must use QRScanner.openSettings() method to guide the user to the settings page
+       // then they can grant the permission from there
+
+     } else {
+
+         // permission was denied, but not permanently. You can ask for permission again at a later time.
+
+	     }
+
+	 })
+
+	 .catch((e: any) => console.log('Error is', e));
+   }
+
+
+
+
+  	ionViewDidLeave() {
+
+    //window.document.querySelector('ion-app').classList.remove('transparentBody')
+  }
+
+
+
+
+
+
+
+
+ 
+
+leer(){
+
+	alert('Entre...')
+
+	window.document.querySelector('ion-app').classList.add('transparentBody')
+
+
+
+
+	//this.qrScanner.show();
+
+	this.qrScanner.prepare()
+    .then((status: QRScannerStatus) => {
+     if (status.authorized) {
+       // camera permission was granted
+
+       alert('Autorizado....')
+       // start scanning
+       this.scanSub = this.qrScanner.scan().subscribe((text: string) => {
+         alert('Scanned something'+ text);
+
+			this.qrScanner.hide(); // hide camera preview
+			this.scanSub.unsubscribe(); // stop scanning
+
+       });
+
+
+     } else if (status.denied) {
+
+       // camera permission was permanently denied
+       // you must use QRScanner.openSettings() method to guide the user to the settings page
+       // then they can grant the permission from there
+
+     } else {
+
+         // permission was denied, but not permanently. You can ask for permission again at a later time.
+
+	     }
+
+	 })
+
+	 .catch((e: any) => console.log('Error is', e));
+
+
+
+  // Optionally request the permission early
+}
 
 }
